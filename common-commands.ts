d24206792d9fb6b096443265828fbbfb4bab8230
luiggy macias <ferrinluiggy@gmail.com>
2022-07-20 08:37:18 -0500

    fddf

---
+++ common-commands.ts
@@ -0,0 +1,6 @@
+// docker run postgres  ---- la descarga y la ejecuta
+// docker pull ---- descarga la imagen pero no la ejecuta
+// docker images ---- muestra las imagenes que estan en el sistema
+// docker ps: muestra los contenedores que se estan ejecutando
+// docker logs container_name ---- muestra los logs del contenedor, tambien se puede usar el ID del contenedor
+// docker logs -f container_name ---- hace lo mismo que el anterior pero seguira ejecutando hasta que se cierre el contenedor
